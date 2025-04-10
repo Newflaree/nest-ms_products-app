42b56d2578011be0a3023b4364acab794437783d
Camilo López <camiloj.lopez79@gmail.com>
2025-04-10 02:47:10 -0400

    Esquema de validación

--- src/config/envs.ts
+++ src/config/envs.ts
@@ -1,8 +1,23 @@
 import 'dotenv/config';
+import * as joi from 'joi';
 
 
-//TODO: Validar mediante un esquema
+interface EnvVars {
+  PORT: number;
+}
+
+const envSchema = joi.object({
+  PORT: joi.number().required()
+}).unknown( true );
+
+const { error, value } = envSchema.validate( process.env );
+
+if ( error ) {
+  throw new Error( `Config validation error: ${ error.message }` );
+}
+
+const envVars: EnvVars = value;
 
 export const envs = {
-  port: process.env.PORT
+  port: envVars.PORT
 }
